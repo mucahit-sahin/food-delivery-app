af6c6fe5a593727eb8ff4ba9cee4f8cbea69b97d
mucahit-sahin <mucahitsahin786@gmail.com>
2023-02-11 21:27:27 +0300

    Created Header Component and Icons

---
+++ Components/Header.js
@@ -0,0 +1,24 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import Menu from "../assets/icons/Menu";
+import Basket from "../assets/icons/Basket";
+import { useNavigation } from "@react-navigation/native";
+
+const Header = () => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 10,
+      }}
+    >
+      <Menu />
+      <Basket basket={5} onPress={() => navigation.navigate("Basket")} />
+    </View>
+  );
+};
+
+export default Header;
